a6ad580815ad3db8c509bc1775ac6f4a56d3c43d
Desire Barine <desirebarine16@gmail.com>
2025-11-29 15:12:09 +0000

    schema return improvement

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -3,10 +3,11 @@ from sqlmodel import Session
 from app.core.db import get_session
 from app.core.security import get_current_user
 from app.models.user import User
+from app.schemas.user import UserRead
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.get("/me", response_model=User)
+@router.get("/me", response_model=UserRead)
 def read_own_profile(current_user: User = Depends(get_current_user)):
     return current_user
 
